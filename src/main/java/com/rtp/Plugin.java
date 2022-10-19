802f6fe4d2544c72f9283c0bee37dc5c5b03f0c6
mrrevo55 <kwolny31@gmail.com>
2022-10-19 21:50:07 +0200

    Create main class and add bukkit dependency

--- src/main/java/com/rtp/Plugin.java
+++ src/main/java/com/rtp/Plugin.java
@@ -1,5 +1,15 @@
 package com.rtp;
 
-class Plugin {
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
+public class Plugin extends JavaPlugin {
+
+    private static final Logger logger = Logger.getLogger("Minecraft");
+
+    @Override
+    public void onEnable() {
+        logger.info("Enabling Abyss plugin!");
+    }
 }
