8aefeb4ff3cb838dd1d6a0908680eed0f1cabd89
TorresS666 <szymon.bartocha.99@outlook.com>
2022-11-15 18:44:41 +0100

    command executor ---

--- src/main/java/com/rtp/command/AbyssCommandExecutor.java
+++ src/main/java/com/rtp/command/AbyssCommandExecutor.java
@@ -1,12 +1,21 @@
-package com.rtp.commandExecutor;
+package com.rtp.command;
 
+import com.rtp.Plugin;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+import javax.inject.Singleton;
+
+@Singleton
 public class AbyssCommandExecutor implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Abyss")) {
+            Plugin.getApplicationContext().getLogger().info("Abyss open");
+         return true;
+        }
+
         return false;
     }
 }
